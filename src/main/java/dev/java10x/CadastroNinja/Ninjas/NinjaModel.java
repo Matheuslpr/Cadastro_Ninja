9ef48c587a68368cf22206bd13028ae6ed2a9249
Matheus <rmatheusluiz@gmail.com>
2025-12-09 20:47:12 -0300

    feat: Criaçao do NinjaModel, Modelagem da tabela e implementação de anotações Lombok

---
+++ src/main/java/dev/java10x/CadastroNinja/Ninjas/NinjaModel.java
@@ -0,0 +1,44 @@
+package dev.java10x.CadastroNinja.Ninjas;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+//TABELA
+@Entity
+@Table(name = "tb_cadastro")
+
+//CONSTRUTORES
+@NoArgsConstructor
+@AllArgsConstructor
+
+//GETTERS E SETTERS
+@Data
+
+public class NinjaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column (name = "img_url")
+    private String imgUrl;
+
+    @Column (name = "rank")
+    private String rank;
+
+    @Column(name = "idade")
+    private int idade;
+
+    @ManyToOne
+    @JoinColumn(name = "missoes_id")
+    private MissoesModel missoes;
+}
